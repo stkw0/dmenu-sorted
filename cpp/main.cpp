b6a25c7e22df461c0188d958eb93befaf515f1aa
David Roman <davidroman96@gmail.com>
2018-02-19 21:08:44 +0100

    Check for executable files

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -25,23 +25,35 @@ std::vector<const char*> paths() {
     return paths;
 }
 
+bool is_executable(const auto& p) {
+    return (fs::status(p).permissions() & (fs::perms::others_exec | fs::perms::group_exec |
+                                           fs::perms::owner_exec)) != fs::perms::none;
+}
+
 std::vector<std::string> commands() {
     static std::vector<std::string> commands;
     if(commands.size() > 0) return commands;
 
     for(auto& path : paths()) try {
+            if(!is_executable(path)) continue;
+
             for(auto& e : fs::directory_iterator(path))
                 commands.emplace_back(e.path().filename().c_str());
         } catch(...) {
             // Ignore errors for now
         }
 
+    std::sort(commands.begin(), commands.end());
+    commands.erase(std::unique(commands.begin(), commands.end()), commands.end());
+
     return commands;
 }
 
 int main() {
+    std::string home = getenv("HOME");
+
     std::vector<std::pair<std::string, int>> cmd_list;
-    std::ifstream cache("/tmp/cache");
+    std::ifstream cache(home + "/.cache/dmenu_sorted");
     if(cache.is_open()) {
         do {
             std::string cmd;
@@ -59,17 +71,14 @@ int main() {
                   [](std::pair<std::string, int> a, std::pair<std::string, int> b) {
                       return a.second > b.second;
                   });
-    } /*else {
-        for(auto& e : commands()) cmd_list.emplace_back(std::make_pair(e. 0));
-    }*/
+    } else {
+        for(auto& e : commands()) cmd_list.emplace_back(std::make_pair(e, 0));
+    }
 
     const char* const argv[] = {"/usr/bin/dmenu", (const char*)0};
     spawn dmenu(argv);
 
-    if(cmd_list.size() <= 0)
-        for(auto& e : commands()) dmenu.stdin << e << "\n";
-    else
-        for(auto& e : cmd_list) dmenu.stdin << e.first << "\n";
+    for(auto& e : cmd_list) dmenu.stdin << e.first << "\n";
     dmenu.send_eof();
     dmenu.wait();
 
@@ -81,14 +90,14 @@ int main() {
     fputs(s.c_str(), p);
     pclose(p);
 
-    std::ifstream r_cache("/tmp/cache");
+    std::ifstream r_cache(home + "/.cache/dmenu_sorted");
     if(!r_cache.is_open() || r_cache.get(), !r_cache.good()) {
         r_cache.close();
-        std::ofstream w_cache("/tmp/cache");
+        std::ofstream w_cache(home + "/.cache/dmenu_sorted");
         for(auto& e : commands()) w_cache << e << " 0\n";
     } else {
-        fs::remove(fs::path("/tmp/cache"));
-        std::ofstream w_cache("/tmp/cache");
+        fs::remove(fs::path(home + "/.cache/dmenu_sorted"));
+        std::ofstream w_cache(home + "/.cache/dmenu_sorted");
         for(auto& e : cmd_list) {
             if(e.first == s) e.second++;
 
